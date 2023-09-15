8a0a85af49fa7bbe6b075079b699f84c2145611c
Nguyên Văn Kịp <nguyenvk93@gmail.com>
2023-09-15 01:30:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-export default function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!sasaas</h2>
     </div>
   );
-}
+};
+
+export default App;
